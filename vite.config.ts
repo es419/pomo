c7d3e79aef5f8b841875b6bb27df28c9c9461ed5
es419 <74262992+es419@users.noreply.github.com>
2026-09-20 00:35:54 +0300

    Align Pomo theme with icon

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
         start_url: '/pomo/',
         scope: '/pomo/',
         icons: [
-          { src: '/pomo/pwa-192x192-v8.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
-          { src: '/pomo/pwa-512x512-v8.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+          { src: '/pomo/pwa-192x192-v9.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+          { src: '/pomo/pwa-512x512-v9.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       }
     })
